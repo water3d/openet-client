b795b36a1816a51d0b4fa03a077b3bdf1eb295e4
Nick Santos <nickrsan@users.noreply.github.com>
2021-07-20 16:54:29 -0700

    Adding test back in

---
+++ tests/test_basic.py
@@ -0,0 +1,22 @@
+import pytest
+
+import os
+
+import openet
+
+raster_params = {
+    'start_date': '2016-01-01',
+    'end_date': '2016-03-20',
+    'geometry': '-120.72612533471566,37.553211935016215,-120.72612533471566,37.474782294423676,-120.59703597924691,37.474782294423676,-120.59703597924691,37.553211935016215',
+    'filename_suffix': 'client_test',
+    'variable': 'et',
+    'model': 'ensemble',
+    'units': 'metric'
+}
+
+
+def test_basic():
+    client = openet.OpenETClient()
+    client.token = os.environ["OPENET_TOKEN"]
+    client.raster.export(params=raster_params, synchronous=True)
+    print(client.raster.downloaded_raster_paths)

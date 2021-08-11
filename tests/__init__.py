17a017f2926f1923b9e0875ea792920804ae8744
Nick Santos <nickrsan@users.noreply.github.com>
2021-08-10 18:28:51 -0700

    beginning of a test for the geodatabase API

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+FOLDER = os.path.dirname(os.path.abspath(__file__))
+TEST_DATA = os.path.join(FOLDER, "test_data")
--- tests/test_geodatabase.py
+++ tests/test_geodatabase.py
@@ -1,5 +1,17 @@
+import os
 import pytest
+
+from . import TEST_DATA
+
+import openet_client
 
 import geopandas
 
-def test_simple_feature_retrieval()
+
+def test_simple_feature_retrieval(features=os.path.join(TEST_DATA, "simple_features.geojson")):
+	df = geopandas.read_file(features)
+	df["centroid"]
+
+	client = openet_client.OpenETClient()
+	client.token = os.environ["OPENET_TOKEN"]
+	client.geodatabase.get_feature_ids()
